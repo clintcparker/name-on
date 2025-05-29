5b70b6a6b15f5eea9a743e065dfcf90cc0cb6f21
Clint Parker <me@clintparker.com>
2025-05-29 14:42:13 -0700

    New footer (#8)
    * added commit hash footer
    
    * updated footer, removed azurite
    
    * refactored CI/CD

--- name-on-blazor/Program.cs
+++ name-on-blazor/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using name_on_blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// The App component is generated from App.razor and should be available in the global namespace
 builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CommitHashService>();
 
 await builder.Build().RunAsync();
